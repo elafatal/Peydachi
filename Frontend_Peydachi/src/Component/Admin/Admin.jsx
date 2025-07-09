e202b0070f53b4270c38861bf3d003bceeecb1b7
elafatal <vahediielahe@gmail.com>
2025-07-09 16:28:50 +0330

    clean admin page

--- Frontend_Peydachi/src/Component/Admin/Admin.jsx
+++ Frontend_Peydachi/src/Component/Admin/Admin.jsx
@@ -1,21 +1,10 @@
 import React, { useState,useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './AdminMenu/Sidebar';
-import TopHeader from './AdminMenu/TopHeader';
-import DashboardOverview from './AdminDashboard/DashboardOverview';
-import StoreManagement from './AdminStore/StoreManagement';
-import axiosInstance from '../axiosInstance';
 import { useAuth } from '../AuthContext/AuthContext';
 import UnauthorizedPage from '../Error/UnauthorizedPage';
-import CityManagement from './AdminCity/CityManagement';
-import UserManagement from './AdminUser/UserManagement';
-import StoreCommentManagement from './AdminCommet/StoreCommentManagement';
 const AdminPage = () => {
   const { role } = useAuth(); 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState('dashboard');
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
   return (
    <>{role === 'admin' || role=== 'superadmin' ? <div className="flex max-h-screen bg-gray-50" dir="rtl">
     <Sidebar />
