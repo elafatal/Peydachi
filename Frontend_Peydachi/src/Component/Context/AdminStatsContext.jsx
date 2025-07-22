7ad553cdc23a34b95ea45c562ea22374ba21d7ea
elafatal <vahediielahe@gmail.com>
2025-07-22 17:51:48 +0330

    add role to admin context

--- Frontend_Peydachi/src/Component/Context/AdminStatsContext.jsx
+++ Frontend_Peydachi/src/Component/Context/AdminStatsContext.jsx
@@ -7,6 +7,7 @@ export const useAdminStats = () => useContext(AdminStatsContext);
 
 export const AdminStatsProvider = ({ children }) => {
   const { user } = useAuth();
+  const { role } = useAuth();
   const [stats, setStats] = useState({
     requests: 0,
     pending: 0,
@@ -16,12 +17,14 @@ export const AdminStatsProvider = ({ children }) => {
 
   const fetchStats = async () => {
     try {
-      const res = await axiosInstance.get('/admin/statistics/get_add_store_request_and_report_stats');
+      if (role === 'admin' || role==='superadmin') {
+        const res = await axiosInstance.get('/admin/statistics/get_add_store_request_and_report_stats');
       const data = res.data;
       const requests = data.total_pending_review_add_store_requests || 0;
       const pending = data.total_pending_review_reports || 0;
       const totalAlerts = requests + pending;
       setStats({ requests, pending, totalAlerts });
+      }
     } catch (err) {
       console.error('خطا در واکشی آمار:', err);
     } finally {
@@ -31,7 +34,7 @@ export const AdminStatsProvider = ({ children }) => {
 
   useEffect(() => {
     if (!user) {
-      setLoading(false); // فقط یک‌بار اجرا می‌شه
+      setLoading(false);
       return;
     }
     fetchStats();
