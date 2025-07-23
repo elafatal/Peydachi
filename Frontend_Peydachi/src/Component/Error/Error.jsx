4d187368ff7a252eb83c08737b23d04fb1d76970
elafatal <vahediielahe@gmail.com>
2025-07-23 20:24:15 +0330

    fix error page

--- Frontend_Peydachi/src/Component/Error/Error.jsx
+++ Frontend_Peydachi/src/Component/Error/Error.jsx
@@ -10,24 +10,23 @@ const ErrorPage = () => {
 
     return (
         <>
-        
             <main>
-        <section class="bg-gray-950 relative max-w-screen  flex justify-center items-center h-screen ">
-            <div class="mx-auto  px-4 sm:px-6 lg:px-8 relative">
+                <section class="bg-gray-950 relative max-w-screen  flex justify-center items-center h-[62vh] ">
+                    <div class="mx-auto  px-4 sm:px-6 lg:px-8 relative">
 
-                <div class=" w-full mx-auto h-auto text-center mb-7">
-                    <div class="w-full h-auto mx-auto py-8 rounded-2xl">
-                        <h1 class="font-serif font-black text-6xl leading-relaxed text-blue-600 text-center mb-6">
-                            4<span class="text-blue-200">0</span>4</h1>
-                        <p dir='rtl' class="font-medium text-xl leading-9 text-gray-100 text-center mb-4">آدرس اشتباهی وارد کردید!</p>
-                        <p class="font-medium text-md leading-9 text-blue-600 text-center mb-6">صفحه ای که به دنبال آن هستید وجود ندارد یا منتقل شده است</p>
-                        <button onClick={handleBackHome} class="w-44 bg-blue-600 text-white rounded-full cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 py-3.5 px-6 text-sm hover:bg-blue-700">
-                          بازگشت به خانه  </button>
+                        <div class=" w-full mx-auto h-auto text-center mb-7">
+                            <div class="w-full h-auto mx-auto py-8 rounded-2xl">
+                                <h1 class="font-serif font-black text-6xl leading-relaxed text-blue-600 text-center mb-6">
+                                    4<span class="text-blue-200">0</span>4</h1>
+                                <p dir='rtl' class="font-medium text-xl leading-9 text-gray-100 text-center mb-4">آدرس اشتباهی وارد کردید!</p>
+                                <p class="font-medium text-md leading-9 text-blue-600 text-center mb-6">صفحه ای که به دنبال آن هستید وجود ندارد یا منتقل شده است</p>
+                                <button onClick={handleBackHome} class="w-44 bg-blue-600 text-white rounded-full cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 py-3.5 px-6 text-sm hover:bg-blue-700">
+                                بازگشت به خانه  </button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </section>
-    </main>
+                </section>
+            </main>
         </>
     );
 };
